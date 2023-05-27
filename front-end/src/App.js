e3f21ed478ba12f7c80ad67b731bb6a980f8714e
Chiang-Yong <chiang_yong@yahoo.com>
2023-05-27 17:22:39 +0800

    Initial release

---
+++ front-end/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RegistrationForm from './components/RegistrationForm';
+//import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    
+          <RegistrationForm />
+
+    </div>
+  );
+}
+
+export default App;
